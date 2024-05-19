ba3e6d5b78cd6cccdd5813e815ebbfd43425ef3e
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-19 23:53:38 +0300

    Added Fourth Project - declaring variables

---
+++ FourthProject/JavaVariables.java
@@ -0,0 +1,26 @@
+package FourthProject;
+
+/*In Java, there are different types of variables, for example:
+
+    String - stores text, such as "Hello". String values are surrounded by double quotes
+    int - stores integers (whole numbers), without decimals, such as 123 or -123
+    float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+    char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+    boolean - stores values with two states: true or false
+ */
+public class JavaVariables {
+    // Syntaxfor variable declaration and initialization
+    // type variableName = value ;
+
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+        int myNum;
+        myNum = 15;
+        System.out.println(myNum);
+        /* use the final keyword to declare constnats */
+
+        final String names = "Darius Mie";
+
+    }
+}
