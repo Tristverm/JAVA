2849de6abd418b084b6442a326c866ec68691e4c
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-19 22:58:10 +0300

    Added Second Project -JAVA syntax

---
+++ SecondProject/JavaSyntax.java
@@ -0,0 +1,11 @@
+package SecondProject;
+
+// Every line of code that runs in Java must be inside a class
+//A class should always start with an uppercase first letter.
+
+public class JavaSyntax {
+    public static void main(){
+System.out.println("Hello there");
+    }
+
+}
