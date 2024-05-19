cee1b23eeaab3b51c60a3b5a0e14cae7901aa7b2
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-19 23:39:39 +0300

    Added Third Project -JAVA syntax

---
+++ ThirdProject/JavaOutput.java
@@ -0,0 +1,23 @@
+package ThirdProject;
+
+/*You can add as many println() methods as you want. 
+* Note that it will add a new line for each method:
+*/
+public class JavaOutput {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        /*
+         * There is also a print() method, which is similar to println().
+         * 
+         * The only difference is that it does not insert a new line at the end of the
+         * output:
+         */
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+
+    }
+}
+
+// When working with strings, they must be wrapped in double quotes
