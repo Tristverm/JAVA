bd8037a7368d3341a87ceda47f8b8e1fa9a28b94
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-20 23:40:53 +0300

    Added Started on strings

---
+++ SeventhProject/Strring.java
@@ -0,0 +1,10 @@
+package SeventhProject;
+
+//The String data type is used to store a sequence of characters (text). String values must be surrounded by double quotes:
+
+public class Strring {
+
+}
+// A String in Java is actually a non-primitive data type, because it refers to
+// an object. The String object has methods that are used to perform certain
+// operations on strings.
