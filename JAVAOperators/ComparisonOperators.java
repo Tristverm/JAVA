aa9b0416e103533851337ce66fd093a2a0f57e93
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-21 10:21:04 +0300

    Added Comparison operators

---
+++ JAVAOperators/ComparisonOperators.java
@@ -0,0 +1,12 @@
+package JAVAOperators;
+
+public class ComparisonOperators {
+    /*
+     * == Equal to x == y
+     * != Not equal x != y
+     * > Greater than x > y
+     * < Less than x < y
+     * >= Greater than or equal to x >= y
+     * <= Less than or equal to x <= y
+     */
+}
