6b0edc4f448f48d0a155eede185cfd4930742cfb
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-21 12:29:02 +0300

    Added Conditionals - switch statement

---
+++ ConditionalStatements/Switch.java
@@ -0,0 +1,35 @@
+package ConditionalStatements;
+
+public class Switch {
+    public static void main(String[] args) {
+        int day = 4;
+        switch (day) {
+            case 9:
+
+                break;
+            case 8:
+
+                break;
+            case 0:
+
+                break;
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4: {
+                System.out.println("Its on Thursday");
+            }
+
+                break;
+
+            default:
+                break;
+        }
+    }
+}
