ed1708873d13cd85332fa2d0987b7f62395b7623
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-21 10:13:21 +0300

    Added arithmetic and Assignment operators

---
+++ JAVAOperators/AssingmentOperators.java
@@ -0,0 +1,17 @@
+package JAVAOperators;
+
+public class AssingmentOperators {
+    /*
+     * =    x = 5   x = 5
+     * +=   x += 3  x = x + 3
+     * -=   x -= 3  x = x - 3
+     * =    x *= 3  x = x * 3
+     * /=   x /= 3  x = x / 3
+     * %=   x %= 3  x = x % 3
+     * &=   x &= 3  x    = x & 3
+     * |=    x |= 3 x = x | 3
+     * ^=    x^= 3  x = x ^ 3
+     * >>=  x >>= 3 x = x >> 3
+     * <<=  x <<= 3 x = x << 3
+     */
+}
