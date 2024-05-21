034d1027f2454b2a773e1a40cd880ed9a728901a
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-21 14:59:49 +0300

    Added Loops -Do  while loop

---
+++ Loops/DoWhile.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class DoWhile {
+    public static void main(){
+        boolean shout = true;
+        do {
+            System.out.println("yes");
+
+            shout= false;
+
+        } while (shout == true);
+    }
+}
