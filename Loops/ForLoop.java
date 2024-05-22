70eba06685eb4b401ca265a4f2fd34a506f65928
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-22 11:11:25 +0300

    Added For loop

---
+++ Loops/ForLoop.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+
+            if (i == 10) {
+                break;
+            } else {
+                continue;
+            }
+
+        }
+    }
+}
