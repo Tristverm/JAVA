f720b93fe850b0b4103cdee16bc89e51cba84b11
Tristverm <emmanuelkyalo72@gmail.com>
2024-05-25 12:21:11 +0300

    Added CLASS and Object CReation

---
+++ CreateObjectAndClass/CreateObjectAndClass.java
@@ -0,0 +1,15 @@
+
+package CreateObjectAndClass;
+
+/**
+ * creating a class called CreateObjectAndClass
+ */
+public class CreateObjectAndClass {
+    final int age = 90;
+
+    // creating an object
+    public static void main(String[] args) {
+        CreateObjectAndClass obj1 = new CreateObjectAndClass();
+        System.out.println(obj1.age);
+    }
+}
